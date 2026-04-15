76ed4be01e97c1ce02a536079c68aea1764ddd3d
Bayesed <h.dinkela@stud.uni-goettingen.de>
2026-04-15 13:33:35 +0200

    fixed indention

--- GRUCovariance.py
+++ GRUCovariance.py
@@ -5,33 +5,32 @@ from vech import unvech
 import numpy as np
 
 class GRUCovariance(nn.Module):
-        def __init__(self, input_size: int, n_assets: int, hidden_size: int = 64, num_layers: int = 2, dropout: float = 0.2):
-            super().__init__()
-            self.input_size = input_size
-            self.n_assets = n_assets
-            self.lower_tri_size = n_assets * (n_assets + 1) // 2
-        
-            self.gru = nn.GRU(
-                input_size=input_size,
-                hidden_size=hidden_size,
-                num_layers=num_layers,
-                batch_first=True,
-                dropout=dropout if num_layers > 1 else 0.0
-            )
-            self.fc = nn.Linear(hidden_size, self.lower_tri_size)
+    def __init__(self, input_size: int, n_assets: int, hidden_size: int = 64, num_layers: int = 2, dropout: float = 0.2):
+        super().__init__()
+        self.input_size = input_size
+        self.n_assets = n_assets
+        self.lower_tri_size = n_assets * (n_assets + 1) // 2
+    
+        self.gru = nn.GRU(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout if num_layers > 1 else 0.0
+        )
+        self.fc = nn.Linear(hidden_size, self.lower_tri_size)
 
-        
-            def forward(self, x: torch.Tensor):
-                # x: (batch, lookback, d)
-                out, _ = self.gru(x) # BxTxh
-                h_last = out[:, -1, :]  # Bxh letzter hidden state
-                raw_covar = self.fc(h_last) # Bx(d(d+1)/2) raw lower-triangular elements
+    def forward(self, x: torch.Tensor):
+        # x: (batch, lookback, d)
+        out, _ = self.gru(x) # BxTxh
+        h_last = out[:, -1, :]  # Bxh letzter hidden state
+        raw_covar = self.fc(h_last) # Bx(d(d+1)/2) raw lower-triangular elements
 
-                L = unvech(raw_covar, self.n_assets) # Bxdxd, lower-triangular
-                diag = F.softplus(torch.diagonal(L, dim1=-2, dim2=-1)) + 1e-4
-                L = torch.tril(L, diagonal=-1) + torch.diag_embed(diag)
-                sigma_t = L @ L.transpose(-1, -2)
-                I = torch.eye(self.n_assets, device=L.device, dtype=L.dtype).unsqueeze(0)
-                sigma_t = sigma_t + 1e-4 * I
-                
-                return sigma_t, L
+        L = unvech(raw_covar, self.n_assets) # Bxdxd, lower-triangular
+        diag = F.softplus(torch.diagonal(L, dim1=-2, dim2=-1)) + 1e-4
+        L = torch.tril(L, diagonal=-1) + torch.diag_embed(diag) 
+        sigma_t = L @ L.transpose(-1, -2)
+        I = torch.eye(self.n_assets, device=L.device, dtype=L.dtype).unsqueeze(0)
+        sigma_t = sigma_t + 1e-4 * I
+        
+        return sigma_t, L
