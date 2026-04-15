9bb1af24bac216584fae484578ad1725700b81b0
Bayesed <h.dinkela@stud.uni-goettingen.de>
2026-04-15 18:53:07 +0200

    added some comments

--- LSTMCovariance.py
+++ LSTMCovariance.py
@@ -20,62 +20,17 @@ class LSTMCovariance(nn.Module):
         )
         self.fc = nn.Linear(hidden_size, self.lower_tri_size)
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, X: torch.Tensor):
         # x: (batch, lookback, d)
-        out, _ = self.lstm(x) # BxTxh
+        out, _ = self.lstm(X) # BxTxh
         h_last = out[:, -1, :]  # Bxh letzter hidden state
         raw_covar = self.fc(h_last) # Bx(d(d+1)/2) raw lower-triangular elements
 
         L = unvech(raw_covar, self.n_assets) # Bxdxd, lower-triangular
-        diag = F.softplus(torch.diagonal(L, dim1=-2, dim2=-1)) + 1e-4
-        L = torch.tril(L, diagonal=-1) + torch.diag_embed(diag)
-        sigma_t = L @ L.transpose(-1, -2)
-        I = torch.eye(self.n_assets, device=L.device, dtype=L.dtype).unsqueeze(0)
-        sigma_t = sigma_t + 1e-4 * I
+        diag = F.softplus(torch.diagonal(L, dim1=-2, dim2=-1)) + 1e-4 # pos. Diagonale erzwingen (Bedingung für Cholesky-Zerlegung)
+        L = torch.tril(L, diagonal=-1) + torch.diag_embed(diag) # sichert untere Dreiecksstruktur und addiert positiv transformierte Diagonale
+        sigma_t = L @ L.transpose(-1, -2) # Bxdxd, symmetrisch und PSD durch Konstruktion
+        I = torch.eye(self.n_assets, device=L.device, dtype=L.dtype).unsqueeze(0) # (1,d,d) Einheitsmatrix für numerische Stabilität
+        sigma_t = sigma_t + 1e-4 * I # jitter für numerische Stabilität
         
         return sigma_t, L
-
-
-def gaussian_nll(y: torch.Tensor, L: torch.Tensor) -> torch.Tensor:
-    """
-    y: (batch, d)
-    chol: (batch, d, d), lower-triangular Cholesky factor of Sigma
-
-    Returns per-sample negative log-likelihood for N(0, Sigma).
-    """
-    d = y.shape[-1] # 
-    y_col = y.unsqueeze(-1) # Bxdx1
-
-    # Solve Sigma^{-1}y via two triangular solves: L z = y, L^T x = z
-    z = torch.linalg.solve_triangular(L, y_col, upper=False) # z = L^{-1} y
-    precision_y = torch.linalg.solve_triangular(L.transpose(-1, -2), z, upper=True).squeeze(-1)
-    mahal = (y * precision_y).sum(dim=-1) # y^T Sigma^{-1} y
-
-    logdet = 2.0 * torch.log(torch.diagonal(L, dim1=-2, dim2=-1)).sum(dim=-1) # log(det(Sigma)) = log(det(L @ L^T)) = log(det(L)^2) = 2*log(det(L)) = 2*sum(log(diag(L)))
-    const = d * np.log(2.0 * np.pi)
-
-    return 0.5 * (const + logdet + mahal)
-
-def student_nll(y: torch.Tensor, L: torch.Tensor, nu: float) -> torch.Tensor:
-    """
-    y: (batch, d)
-    chol: (batch, d, d), lower-triangular Cholesky factor of Sigma
-    nu: degrees of freedom
-
-    Returns per-sample negative log-likelihood for multivariate Student-t with mean 0, scale Sigma, and nu degrees of freedom.
-    """
-    d = y.shape[-1]
-    y_col = y.unsqueeze(-1)
-
-    z = torch.linalg.solve_triangular(L, y_col, upper=False) # z = L^{-1} y
-    precision_y = torch.linalg.solve_triangular(L.transpose(-1, -2), z, upper=True).squeeze(-1) # x = L^{-T} z = L^{-T} L^{-1} y = Sigma^{-1} y
-    mahal = (y * precision_y).sum(dim=-1) # y^T Sigma^{-1} y
-
-    logdet = 2.0 * torch.log(torch.diagonal(L, dim1=-2, dim2=-1)).sum(dim=-1) # log(det(Sigma)) = 2*sum(log(diag(L)))
-    
-    const = (
-        torch.lgamma((nu + d) / 2.0) - torch.lgamma(nu / 2.0) - 
-        0.5 * d * np.log(nu * np.pi) - logdet
-    )
-
-    return -const + 0.5 * (nu + d) * torch.log1p(mahal / nu)
